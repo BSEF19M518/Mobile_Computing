01020e2291f78e1613032b20c836a64a4f6381cf
Waleed <bsef19m518@pucit.edu.pk>
2022-04-19 22:27:27 +0500

    Task02 (button click listener attached to textView)

--- BSEF19M518_App1/app/src/main/java/com/example/bsef19m518_app1/MainActivity.java
+++ BSEF19M518_App1/app/src/main/java/com/example/bsef19m518_app1/MainActivity.java
@@ -4,13 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    TextView textView;
+
+    boolean b=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button=findViewById(R.id.button);
+        textView=findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(b) {
+                    b=false;
+                    textView.setText("BSEF19M518");
+                }
+                else{
+                    b=true;
+                    textView.setText("Click button below:");
+                }
+            }
+        });
     }
 }
