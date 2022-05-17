ae73d978a4c38557239a6e7a9aaa9cdf66e36a52
Waleed <bsef19m518@pucit.edu.pk>
2022-05-17 23:02:34 +0500

    Implicit and Explicit intent practice

--- ExplicitIntentPractice/app/src/main/java/com/example/explicitintentpractice/MainActivity.java
+++ ExplicitIntentPractice/app/src/main/java/com/example/explicitintentpractice/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
     EditText optionText;
+    String[] correct_options;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +25,27 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 optionText=findViewById(R.id.optionText);
                 String option=optionText.getText().toString();
-                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
-                intent.putExtra("option",option);
-                startActivity(intent);
+
+                correct_options=new String[] {"1","2","3"};
+
+                if(inOptions(option)) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    intent.putExtra("option", option);
+                    startActivity(intent);
+                }
+                else
+                    Toast.makeText(MainActivity.this, "Please enter valid option", Toast.LENGTH_SHORT).show();
+
             }
         });
+    }
+    private boolean inOptions(String option)
+    {
+        for(int i=0;i<correct_options.length;i++) {
+            if (option.equals(correct_options[i]))
+                return true;
+        }
+            return false;
     }
 
 }
--- ImplicitIntentPractice/app/src/main/java/com/example/implicitintentpractice/MainActivity.java
+++ ImplicitIntentPractice/app/src/main/java/com/example/implicitintentpractice/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.implicitintentpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void call_us(View v)
+    {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + "03174458930"));
+        startActivity(intent);
+    }
+    public void email_us(View v)
+    {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, "waleedmustafa50@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Application feedback");
+        startActivity(intent);
+
+    }
+    public void locate_us(View v)
+    {
+        Uri geo = Uri.parse("geo:31.567207, 74.289705");
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geo);
+        startActivity(intent);
+    }
+    public void web_visit(View v)
+    {
+        Uri webpage = Uri.parse("http://waleedmustafa50.pythonanywhere.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+            startActivity(intent);
+
     }
 }
